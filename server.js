ad4932a967be5e3bd4aed3e8fa40b1770c44ceab
nicolaslimaalbuquerque <Nicolas.albuquerquerp@gmail.com>
2025-08-12 09:25:57 -0300

    isso

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 import express from 'express'
+
+// objeto da biblioteca
 const app = express()
 
-app.use 
+//indica q vai usar o json
+app.use(express.json())
 
-const usuarios =[]
+//vetor pra guardar antes de ter o banco
+const usuarios = []
 
-// Rotas
-app.get('/cadastro', (req,res)=>{
-    res.send('DEU CERTO COM O GET')
+// rotas
+app.get('/cadastro', (req, res) => {
+    //res.send('tudo ok com get') //parametro de resposta
+    res.status(200).json(usuarios)
+}) //rota get, coloca 'como pega ela', funcao call back (requisicao, reposta)
+
+app.post('/cadastro', (req, res) => {
+    // console.log(req.body)
+    usuarios.push(req.body)
+    //res.status(201).send('tudo ok com post')
+    res.status(201).json(req.body)
 })
 
-app.post('/cadastro', (req,res)=>{
-    console.log(req.body);
-    res.send('DEU CERTO COM O POST')
-})
-
-
-//Porta local servidor
-app.listen(3000,()=>{
-    console.log('SERVER TA RODANDO!')
-})
+// porta local do servidor
+app.listen(3000, () => {
+    console.log('servidor rodando')
+}) //fica de olho na porta, pode trabalhar com 2 parametros porta, call back
